56c78e963eea5a993ec80677b15a943f6f3b58ff
Niranjay Ajayan <128250840+dyingpotato890@users.noreply.github.com>
2024-06-17 02:29:53 +0530

    Added xor gate

---
+++ xor_gate/xor_gate.srcs/sources_1/new/xor_gate.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 17.06.2024 01:38:15
+// Design Name: 
+// Module Name: xor_gate
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module xor_gate(a,b,y);
+
+input a,b;
+output y;
+
+xor u1(y,a,b);
+endmodule
